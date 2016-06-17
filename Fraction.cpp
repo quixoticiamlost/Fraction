40a9896ab1c6f12e69a3d423667dc19e0bf3861a
quixoticiamlost <quixoticiamlost@gmail.com>
2016-06-17 13:30:45 -0700

    Update Fraction.cpp

--- Fraction.cpp
+++ Fraction.cpp
@@ -10,19 +10,25 @@ Fraction::Fraction():m_numerator(0), m_denominator(1){}
 Fraction::Fraction(int numerator, int denominator)
 {
 	using pmath;
-	// input is positive
+	// input is positive : erase possible negative signs
 	if ((!is_neg(numerator))&&(!is_neg(denominator)) || (is_neg(numerator)) && (is_neg(denominator))
 	{	
-		this->m_numerator = reduceabs(numerator)
+		this->m_numerator = abs(numerator);
+		this->m_denominator = abs(denominator);
 	}
-	// else it is negative 
+	// else it is negative: erase possible negatives and store negative sign in numerator 
 	else 
 	{
+		this->m_numerator = -abs(numerator);
+		this->m_denominator = abs(denominator);
 	}
+	reduce();
 }
 
-void Fraction::reduce(int numerator, int denominator)
+void Fraction::reduce()
 {
-		
+	int factor = pmath::gcf(this->m_numerator, this->m_denominator);
+	this->m_numerator / factor;
+	this->m_denominator /= factor;
 }
 #endif
